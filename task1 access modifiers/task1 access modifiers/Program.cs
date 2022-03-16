0c52c1908055779912b730dc71fe097b61bedca6
royasulthani <99806068+royasulthani@users.noreply.github.com>
2022-03-16 01:45:25 -0700

    Add files via upload

---
+++ task1 access modifiers/task1 access modifiers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace task1_access_modifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ task1 access modifiers/task1 access modifiers/User.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task1_access_modifiers
+{
+    internal class User
+    {
+        public string _username;
+        private string _password;
+        public void Users(string username)
+        {
+            UserName = username;
+        }
+        public string UserName
+        {
+            get
+            {
+                return this._username;
+            }
+            set
+            {
+                if (value.Length >= 6 && value.Length <= 25) ;
+                this._username = value;
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return this._password;
+            }
+            set
+            {
+                if (value.Length >= 8 && value.Length <= 25) ;
+                this._password = value;
+            }
+        }
+    }
+}
+
+              
